049a18b0c3e413e5c87b09086f518ae8611df067
jeremycheongzm <115978571+jeremycheongzm@users.noreply.github.com>
2025-11-20 06:58:11 +0000

    implement get all books function

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,13 +13,18 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books, null, 2));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    return res.send(JSON.stringify(books[isbn], null, 2));
+  } else {
+    return res.status(404).send(JSON.stringify({ error: "Book not found"}, null, 2))
+  }
  });
   
 // Get book details based on author
